a446d0b8c6e6d28420509dfcfd977879fec73a54
okothpius <orukopius8@gmail.com>
2023-10-14 03:21:24 +0300

    feat: init commit

---
+++ src/main/java/com/redhat/developers/FruityViceService.java
@@ -0,0 +1,24 @@
+package com.redhat.developers;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/api/fruit")
+@RegisterRestClient
+public interface FruityViceService {
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Retry(maxRetries = 3, delay = 2000)
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = 0.75, delay = 5000)
+    FruityVice getFruitByName(@PathParam("name") String name);
+
+}
